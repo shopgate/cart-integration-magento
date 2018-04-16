66c453d004bdb7972961291d5203618a9db726cd
Rainer Skistims <rainer.skistims@shopgate.com>
2018-04-16 10:53:32 +0200

    remove unnecessary observer call

--- src/app/code/community/Shopgate/Framework/Model/Payment/Abstract.php
+++ src/app/code/community/Shopgate/Framework/Model/Payment/Abstract.php
@@ -311,7 +311,6 @@ class Shopgate_Framework_Model_Payment_Abstract
         }
 
         Mage::dispatchEvent('checkout_submit_all_after', array('order' => $order, 'quote' => $quote));
-        Mage::dispatchEvent('sales_model_service_quote_submit_after', array('order' => $order, 'quote' => $quote));
 
         return $order;
     }
