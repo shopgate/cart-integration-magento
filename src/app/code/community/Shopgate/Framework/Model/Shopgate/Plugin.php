6e8e93ca71ebd27c88ca4de79086805dc9e8b775
André Kraus <andre.kraus@shopgate.com>
2018-05-04 09:46:54 +0200

    MAGENTO-2018: fixed missing shipping methods in cart validation for c… (#20)
    * MAGENTO-2018: fixed missing shipping methods in cart validation for configurable products
    
    * MAGENTO-2018: Fixed naming of variable

--- src/app/code/community/Shopgate/Framework/Model/Shopgate/Plugin.php
+++ src/app/code/community/Shopgate/Framework/Model/Shopgate/Plugin.php
@@ -628,6 +628,10 @@ class Shopgate_Framework_Model_Shopgate_Plugin extends ShopgatePlugin
 
                 if (!is_null($productWeight)) {
                     $quoteItem->setWeight($productWeight);
+
+                    foreach ($quoteItem->getChildren() as $child) {
+                        $child->setWeight($productWeight);
+                    }
                 }
                 $quoteItem->setRowWeight($quoteItem->getWeight() * $quoteItem->getQty());
                 $quoteItem->setWeeeTaxApplied(serialize(array()));
